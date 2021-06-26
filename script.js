acd1ad03b768e1572f150fbc3c9721dd6b7d3f0c
Gabriel Finatti <ghfinatti3@gmail.com>
2021-06-26 13:15:11 -0300

    edit script.js styles.css

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const container = document.querySelector('.gridcontainer');
 
-for (let i = 1; i <= 16; i++){
-    const square = document.createElement('div');
-    square.classList.add(`square`)
-    container.appendChild(square);
+
+
+function resetGrid(){
+    let pixels = document.querySelectorAll('.square');
+    for (const pixel of pixels){
+        pixel.style.backgroundColor = "white";
+    }
 };
 
-const pixels = document.querySelectorAll('.square');
+function gridSize(sideSize){
+    const container = document.querySelector('.gridcontainer');
+    container.style.gridTemplateColumns = `repeat(${sideSize}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${sideSize}, 1fr)`
+    for (let i = 1; i <= sideSize**2; i++){
+        const square = document.createElement('div');
+        square.classList.add(`square`)
+        container.appendChild(square);
+    };
+};
+
+
+const slider = document.querySelector('#myRange');
+slider.addEventListener('mouseup', function(){
+    resetGrid();
+    gridSize(slider.value);
+});
+
+
+gridSize(slider.value);
+
+let pixels = document.querySelectorAll('.square');
 for(const pixel of pixels){
-    pixel.addEventListener("mouseenter", function(e){
+    pixel.addEventListener("mouseover", function(e){
         e.currentTarget.style.backgroundColor = "black";
     });
-}
+};
+
+const resetButton = document.getElementById('resetbtn')
+resetButton.addEventListener("click", function(){
+    resetGrid()
+});
